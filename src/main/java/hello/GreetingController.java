5e2e7a4d0f1c38974461ec9864ddfbd1c21f4fb1
marsonto <marius.sontopski@web.de>
2019-03-01 15:13:15 +0100

    throws own exception when numbers or special characters are put into string

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,12 +21,23 @@ public class GreetingController {
 	}
 
 	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, service.uppercase(name)));
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws NoLetterException {
+		if(name.matches("[a-zA-Z]+")) {
+			return new Greeting(counter.incrementAndGet(), String.format(template, service.uppercase(name)));
+		}
+		else {throw new NoLetterException();}
 	}
 	
 	@RequestMapping("/greeting/{name}")
-	public Greeting pathGreeting(@PathVariable("name") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, service.uppercase(name)));
+	public Greeting pathGreeting(@PathVariable("name") String name) throws NoLetterException {
+		if(name.matches("[a-zA-Z]+")) {
+			return new Greeting(counter.incrementAndGet(), String.format(template, service.uppercase(name)));
+		}
+		else {throw new NoLetterException(); }
+	}
+	
+	@ExceptionHandler(NoLetterException.class)
+	public String handle() {
+		return "Numbers and special characters are not allowed!";
 	}
 }
