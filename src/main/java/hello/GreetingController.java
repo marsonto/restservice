1923d6c3606c18de3e0fa0847023c35c7b95a619
marsonto <marius.sontopski@web.de>
2019-03-01 14:15:14 +0100

    RequestMapping with PathVariable

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,11 @@ public class GreetingController {
 
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, service.uppercase(name)));
+	}
+	
+	@RequestMapping("/greeting/{name}")
+	public Greeting pathGreeting(@PathVariable("name") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, service.uppercase(name)));
 	}
 }
