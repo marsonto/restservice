aee3ec3b2eb6fd3c63df3053277ac4e19a7d25ec
marsonto <marius.sontopski@web.de>
2019-03-15 09:44:33 +0100

    database connection working without jar

--- src/main/java/hello/databaseConnector.java
+++ src/main/java/hello/databaseConnector.java
@@ -11,23 +11,12 @@ public class databaseConnector
         System.out.println("\n\n***** MySQL JDBC Connection Testing *****");
         Connection conn = null;
         try {
-            Class.forName ("com.MySQL.jdbc.Driver").newInstance ();
-            conn = DriverManager.getConnection ("jdbc:MySQL://localhost/mydb", "marsonto", "123456");
+            //Class.forName ("com.MySQL.jdbc.Driver").newInstance ();
+            conn = DriverManager.getConnection ("jdbc:MySQL://localhost/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "marsonto", "123456");
             System.out.println ("\nDatabase Connection Established...");
         } catch (Exception ex) {
             System.err.println ("Cannot connect to database server");
             ex.printStackTrace();
-        } finally {
-            if (conn != null) {
-                try {
-                    System.out.println("\n***** Let terminate the Connection *****");
-                    conn.close ();
-                    System.out.println ("\nDatabase connection terminated...");
-                }
-                catch (Exception ex) {
-                    System.out.println ("Error in connection termination!");
-                }
-            }
         }
     }
 }
