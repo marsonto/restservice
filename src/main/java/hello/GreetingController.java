b74adb853707c363bfc129428941bda6837a8661
marsonto <marius.sontopski@web.de>
2019-03-15 16:08:55 +0100

    get data from yaml file

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,5 @@
 package hello;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
--- src/main/java/hello/databaseConnector.java
+++ src/main/java/hello/databaseConnector.java
@@ -1,18 +1,23 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 @Service
 public class databaseConnector
 {
+    @Value("${user}")
+    private String user;
+    @Value("${password}")
+    private String password;
+
     public Connection connectToData(){
         System.out.println("\n\n***** MySQL JDBC Connection Testing *****");
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection ("jdbc:MySQL://localhost/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "marsonto", "123456");
+            conn = DriverManager.getConnection ("jdbc:MySQL://localhost/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", user, password);
             System.out.println ("\nDatabase Connection Established...");
         } catch (Exception ex) {
             System.err.println ("Cannot connect to database server");
