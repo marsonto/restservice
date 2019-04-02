38231a5f7932380ef0e2ab09a51073a223f2eae3
marsonto <marius.sontopski@web.de>
2019-04-02 10:31:58 +0200

    stuff moved

--- src/main/java/hello/DrinkRepository.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DrinkRepository extends CrudRepository<Drink, Integer> {
-}
--- src/main/java/hello/NoLetterException.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class NoLetterException extends Exception{
-	public NoLetterException() {
-		super();
-	}
-}
